ca11b5ca2bfad4633f9c9b7a2f4df5949348d199
Ajeet Kumar Jha <84560142+Ajeetjha123@users.noreply.github.com>
2023-05-28 12:23:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,52 @@ function saveInput(event) {
     phone: document.getElementById('phone').value,
   };
 
-  axios.post('https://crudcrud.com/api/appominent/6472f6d9786dbb03e878ce90', formValues)
+  axios.post('https://crudcrud.com/api/6472f6d9786dbb03e878ce90', formValues)
     .then(response => {
       console.log(response.data); // Optional: Display the response data in the console
-      displayFormValues();
+      displayFormValues(response.data);
     })
     .catch(error => {
       console.error(error);
     });
 }
 
+function displayFormValues(savedValues) {
+  const displayArea = document.createElement('div');
+  const namePara = document.createElement('p');
+  const emailPara = document.createElement('p');
+  const phonePara = document.createElement('p');
+  namePara.textContent = `Name: ${savedValues.name}`;
+  emailPara.textContent = `Email: ${savedValues.email}`;
+  phonePara.textContent = `Phone: ${savedValues.phone}`;
+  displayArea.appendChild(namePara);
+  displayArea.appendChild(emailPara);
+  displayArea.appendChild(phonePara);
+
+  const existingDisplayArea = document.getElementById('displayArea');
+  if (existingDisplayArea) {
+    existingDisplayArea.remove(); // Remove the existing display area if it exists
+  }
+
+  displayArea.id = 'displayArea'; // Set an ID for the display area
+  document.body.appendChild(displayArea);
+}
+
+window.onload = function() {
+  if (localStorage.getItem('formValues')) {
+    const savedValues = JSON.parse(localStorage.getItem('formValues'));
+    displayFormValues(savedValues);
+  }
+
+  axios.get('https://crudcrud.com/api/6472f6d9786dbb03e878ce90')
+    .then(response => {
+      const data = response.data;
+      if (data) {
+        localStorage.setItem('formValues', JSON.stringify(data));
+        displayFormValues(data);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
